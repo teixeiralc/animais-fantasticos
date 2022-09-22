85e981dd3e4a9893c4a7206bf4bb25dfdb556f61
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-09-22 15:25:25 -0300

    login modal with js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from "./modules/smooth-scroll.js";
 import initAnimationOnScroll from "./modules/animation-at-scroll.js";
 import initAccordion from "./modules/accordion-list.js";
 import initTabMenuNav from "./modules/tabNav.js";
+import initModal from "./modules/modal.js";
 
 initSmoothScroll();
 initAnimationOnScroll();
 initAccordion();
 initTabMenuNav();
+initModal();
