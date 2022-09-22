a2f301f27f1240e03b6bbf236c829b26ff08a16c
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-09-22 17:02:28 -0300

    tooltip map

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimationOnScroll from "./modules/animation-at-scroll.js";
 import initAccordion from "./modules/accordion-list.js";
 import initTabMenuNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initSmoothScroll();
 initAnimationOnScroll();
 initAccordion();
 initTabMenuNav();
 initModal();
+initTooltip();
