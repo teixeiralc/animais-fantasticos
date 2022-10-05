2a1201ac485fa389f442f1ab8b512daa5435c1fa
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-10-05 09:26:41 -0300

    add babel and compile js code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-import initSmoothScroll from "./modules/smooth-scroll.js";
-import initAnimationOnScroll from "./modules/animation-at-scroll.js";
-import initAccordion from "./modules/accordion-list.js";
-import initTabMenuNav from "./modules/tabNav.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
-import initMobileMenu from "./modules/mobile-menu.js";
-import initOpeningHours from "./modules/opening-hours.js";
-import initAnimalsApi from "./modules/fetch-animais.js";
-import initBitcoinApi from "./modules/fetch-btc.js";
+import initSmoothScroll from './modules/smooth-scroll.js';
+import initAnimationOnScroll from './modules/animation-at-scroll.js';
+import initAccordion from './modules/accordion-list.js';
+import initTabMenuNav from './modules/tabNav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMobileMenu from './modules/mobile-menu.js';
+import initOpeningHours from './modules/opening-hours.js';
+import initAnimalsApi from './modules/fetch-animais.js';
+import initBitcoinApi from './modules/fetch-btc.js';
 
 initSmoothScroll();
 initAnimationOnScroll();
