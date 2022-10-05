b415c67772618a499025611dcce44442d8f0456a
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-10-05 14:57:26 -0300

    fix mobile menu bug

--- js/modules/mobile-menu.js
+++ js/modules/mobile-menu.js
@@ -1,16 +1,17 @@
-import outsideClick from "./outside-click.js";
+import outsideClick from './outside-click.js';
 
 export default function initMobileMenu() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const events = ["click", "touchstart"];
+  const events = ['click', 'touchstart'];
 
-  function openMenu() {
-    menuButton.classList.add("active");
-    menuList.classList.add("active");
+  function openMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    menuButton.classList.add('active');
+    menuList.classList.add('active');
     outsideClick(menuList, events, () => {
-      menuButton.classList.remove("active");
-      menuList.classList.remove("active");
+      menuButton.classList.remove('active');
+      menuList.classList.remove('active');
     });
   }
 
